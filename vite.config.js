0f7f058732e04fda887369325e1b4a32f7d79e82
afiqhazimabdaziz <afiqhazimabdaziz@gmail.com>
2026-07-08 23:07:02 +0800

    remove

--- vite.config.js
+++ vite.config.js
@@ -8,28 +8,6 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-            refresh: true,
         }),
     ],
-    // Use relative paths for production
-    base: process.env.APP_ENV === 'production' ? '/build/' : '/',
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        emptyOutDir: true,
-        rollupOptions: {
-            output: {
-                assetFileNames: 'assets/[name]-[hash][extname]',
-                chunkFileNames: 'js/[name]-[hash].js',
-                entryFileNames: 'js/[name]-[hash].js',
-            },
-        },
-    },
-    server: {
-        https: false, // Set to false for production
-        host: '0.0.0.0',
-        hmr: {
-            host: 'localhost',
-        },
-    },
 });

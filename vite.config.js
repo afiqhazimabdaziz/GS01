b474f871b9001c4df8728e826f8df1cfac32a117
afiqhazimabdaziz <afiqhazimabdaziz@gmail.com>
2026-07-08 21:59:34 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
+            // Important for production
+            buildDirectory: 'build',
         }),
     ],
     build: {
@@ -18,10 +23,5 @@ export default defineConfig({
                 assetFileNames: 'assets/[name].[hash].[ext]',
             },
         },
-    },
-    // Important for Railway
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
     },
 });

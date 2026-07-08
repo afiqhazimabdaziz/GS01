3dd28d78ea44981111d704b0d2f13667ea48e066
afiqhazimabdaziz <afiqhazimabdaziz@gmail.com>
2026-07-08 21:56:32 +0800

    add config for railway

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,20 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].[hash].js',
+                chunkFileNames: 'assets/[name].[hash].js',
+                assetFileNames: 'assets/[name].[hash].[ext]',
+            },
+        },
+    },
+    // Important for Railway
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+    },
 });
